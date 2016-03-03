35420d4f91e55f15f7068b6eda83dd5229c3717f
Jurn Ho <jurn@magicmonster.com>
2016-03-03 21:04:56 +0000

    hello

---
+++ frontend/src/cgi/hello.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use CGI;
+$cgi = new CGI;
+print $cgi->header();
+print <<__EOF;
+<html>
+<head>
+   <title>Hello World!</title>
+</head>
+<body>
+<h1>Hello World!</h1>
+</body>
+</html>
+__EOF
