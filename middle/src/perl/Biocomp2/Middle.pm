b9f84b75e06984bc74c0bc969ba0627251d48893
Jurn Ho <jurn@magicmonster.com>
2016-03-10 20:02:03 +0000

    talk to data access module

--- middle/src/perl/Biocomp2/Middle.pm
+++ middle/src/perl/Biocomp2/Middle.pm
@@ -1,9 +1,11 @@
 package Biocomp2::Middle;
 use strict;
 use warnings;
+use Biocomp2::DataAccess;
 
 sub hello {
-  return "testing6666";
+  my $ggg = Biocomp2::DataAccess::bye();
+  return "testing6666  $ggg";
 }
 
 1;
