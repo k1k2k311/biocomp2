0d2f3d03907a901dc98c08662a010404cf6dd7bb
Igor Ruiz de los Mozos <i.mozos@ucl.ac.uk>
2016-03-12 12:29:58 +0000

    Open and read the file

--- database/src/parse.pl
+++ database/src/parse.pl
@@ -1,1 +1,29 @@
-test
+#!/usr/bin/perl
+use strict;
+use warnings;
+use DBI;
+
+my $cmd = 'wget http://www.bioinf.org.uk/teaching/bbk/biocomp2/data/chrom_CDS_16.gz';
+system($cmd);
+
+my $cmd = 'gunzip chrom_CDS_16.gz';
+system($cmd);
+
+
+# Open data file 
+my $data_file = "chrom_CDS_16";
+chomp $data_file;
+open (DATA, $data_file) or die "Can't open $!, closing";
+
+while ( my $gb_file = <DATA>){
+
+	print $gb_file;	
+
+}
+
+
+close DATA;
+
+
+my $cmd = 'rm chrom_CDS_16';
+system($cmd);
