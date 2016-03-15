4b47ab606324516594bb2dd8ea673c613a597f23
Igor Ruiz_De_Los_Mozos <ri001@hope.cryst.bbk.ac.uk>
2016-03-15 20:19:27 +0000

    Add dummy data to subroutine database

--- database/src/perl/Biocomp2/DataAccess.pm
+++ database/src/perl/Biocomp2/DataAccess.pm
@@ -10,9 +10,11 @@ sub get_gene_names {
 	my %gene_names;
 	#SELECT gene_ID, acc_ver  FROM chromosome16_genes;
 	## EXAMPLE ## $gene_names{'$gene_ID'}='$acc_ver'
-	return %gene_names;
+	$gene_names{'457686'}='dumygene';
+	$gene_names{'45764556'}='dumygene2';
 	print "Its working";
-
+	return %gene_names;
+	
 }
 
 1; 
