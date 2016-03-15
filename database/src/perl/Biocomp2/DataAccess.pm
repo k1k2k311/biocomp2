ef520b0291918e65be443f1c0bb24dd4022a9639
Igor Ruiz_De_Los_Mozos <ri001@hope.cryst.bbk.ac.uk>
2016-03-15 20:55:28 +0000

    Change subroutine DataAccess talk to mysql

--- database/src/perl/Biocomp2/DataAccess.pm
+++ database/src/perl/Biocomp2/DataAccess.pm
@@ -1,18 +1,42 @@
 package Biocomp2::DataAccess;
 use strict;
 use warnings;
+use DBI;
+
+
 
 sub bye {
   return "validating 44444";
 }
 
 sub get_gene_names {
+
+	
+	my $dbname   = "ri001";
+	my $dbhost   = "hope.cryst.bbk.ac.uk";				#hope.cryst.bbk.ac.uk
+	my $dbsource = "dbi:mysql:database=$dbname;host=$dbhost";
+	my $username = "ri001";
+	my $password = "6xu1ornxo";
+
+	my $dbh = DBI->connect($dbsource, $username, $password) or die "Imposible conect to DataBase \n";
+
+	my $sql = 'SELECT gene_ID, acc_ver  FROM chromosome16_genes';
+	my $gene_ID = '';
+	my $acc_ver = '';
 	my %gene_names;
-	#SELECT gene_ID, acc_ver  FROM chromosome16_genes;
-	## EXAMPLE ## $gene_names{'$gene_ID'}='$acc_ver'
-	$gene_names{'457686'}='dumygene';
-	$gene_names{'45764556'}='dumygene2';
-	print "Its working";
+
+    my $sth = $dbh->prepare($sql);
+    $sth->execute;
+    
+    while(($gene_ID, $acc_ver) = $sth->fetchrow_array)
+    {
+    	#print "$gene_ID, $acc_ver \n";
+
+		$gene_names{$gene_ID}=$acc_ver;
+
+	
+	}
+
 	return %gene_names;
 	
 }
