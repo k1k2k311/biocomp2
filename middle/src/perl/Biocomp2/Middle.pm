cf880662b8552210dfba4ba4c83eb7f64bfc3436
Jurn Ho <jurn@magicmonster.com>
2016-03-15 20:20:50 +0000

    call datalayer

--- middle/src/perl/Biocomp2/Middle.pm
+++ middle/src/perl/Biocomp2/Middle.pm
@@ -9,10 +9,8 @@ sub hello {
 }
 
 sub get_all_genes {
-  my %all_genes;
-  $all_genes{'60391390'}='MTG16';
-  $all_genes{'3550832'}='TSC2';
-  return %all_genes;
+  my %gene_names = Biocomp2::DataAccess::get_gene_names();
+  return %gene_names;
 }
 
 1;
