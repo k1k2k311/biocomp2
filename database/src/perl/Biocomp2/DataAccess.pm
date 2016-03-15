9cb2c136a5e2618159516ab95aef287e4cd49e05
Igor Ruiz_De_Los_Mozos <ri001@hope.cryst.bbk.ac.uk>
2016-03-15 20:12:53 +0000

    Add subroutine to query database

--- database/src/perl/Biocomp2/DataAccess.pm
+++ database/src/perl/Biocomp2/DataAccess.pm
@@ -6,5 +6,14 @@ sub bye {
   return "validating 44444";
 }
 
+sub get_gene_names {
+	my %gene_names;
+	#SELECT gene_ID, acc_ver  FROM chromosome16_genes;
+	## EXAMPLE ## $gene_names{'$gene_ID'}='$acc_ver'
+	return %gene_names;
+	print "Its working";
+
+}
+
 1; 
 
