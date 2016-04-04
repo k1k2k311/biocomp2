bd35c4c28cb10e554970fffebad1657a52afcc86
Igor Ruiz de los Mozos <i.mozos@ucl.ac.uk>
2016-04-04 01:10:12 +0100

    Update DataBAse

--- database/src/create.sql
+++ database/src/create.sql
@@ -7,12 +7,29 @@ CREATE TABLE chromosome16_genes
 (
     prim_key	SMALLINT        NOT NULL,
     gene_ID     INT             NOT NULL, 
-    locus       VARCHAR(100)    DEFAULT 'N/A' NOT NULL,
-    accession   VARCHAR(20)     DEFAULT 'N/A' NOT NULL,
-    acc_ver     VARCHAR(20)     DEFAULT 'N/A' NOT NULL,
-	
+    acc_ver     VARCHAR(20)     NOT NULL,
+    gene       	VARCHAR(100)    DEFAULT 'N/A' NOT NULL,
+    map			VARCHAR(20)     DEFAULT 'N/A' NOT NULL,
+    product		VARCHAR(200)    DEFAULT 'N/A' NOT NULL,	
+    protID		VARCHAR(20)     DEFAULT 'N/A' NOT NULL,
+    aa			TEXT    		DEFAULT 'N/A' NOT NULL,	
 
     PRIMARY KEY (prim_key)
 );
+CREATE TABLE sequence
+(
+    prim_key	SMALLINT        NOT NULL,
+    gene_ID     INT             NOT NULL, 
+    cordinates	VARCHAR(100)    NOT NULL,
+    complement	VARCHAR(20)     NOT NULL,
+    sequence	LONGTEXT     	NOT NULL,
+	cod_start	INT	     		NOT NULL,
+
+    PRIMARY KEY (prim_key)
+);
+
+#($gene_ID, $acc_ver, @cordinates, $complement, $gene, $sequence, $map, $cod_start, $product, $protID, $aa)
+
+
 CREATE INDEX locus_idx on chromosome16_genes(locus);
 
