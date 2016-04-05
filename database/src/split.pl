546ab17fc266a5a18b5c169a2d86d8df518e0711
Igor Ruiz de los Mozos <i.mozos@ucl.ac.uk>
2016-04-05 19:42:27 +0100

    Array of coordinates solved

--- database/src/split.pl
+++ database/src/split.pl
@@ -22,7 +22,7 @@ my $flag = 0;
 
 my $entry = '';
 my %gene;
-my %cordinates;
+my %cordinates_hash;
 my $count = 1;
 my $compl_count = 1;
 my $sense_count = 1;
@@ -105,7 +105,7 @@ while ($entry = get_entry($file) ) {
 			#print "COUNT  ", $count++, "\n";
 			$cds = $1;
 			$cds =~ s/(join)*\s*//g;							# Remove "join", lines and spaces
-			#print "CDS  ", $cds, "\n\n";
+			print "CDS  ", $cds, "\n\n";
 			
 			
 			
@@ -204,45 +204,60 @@ my @values = ($acc_ver, $complement, $gene, $sequence, $map, $cod_start, $produc
 $gene{$gene_ID} = [ @values ];
 
 
-$cordinates{$gene_ID} = [ @cordinates ];
+$cordinates_hash{$gene_ID} = [ @cordinates ];
 		
 	
 } 
 
-foreach my $key (keys %gene)
-{
-	#print "key $key value $genes{$key} \n";
-	#print "key $key value1 @{ $genes{$key} } \n"
-	print "Gene ID			", $key, "\n";
-	my $acc_ver = @{ $gene{$key} }[0];
-	print "AccesionVersion		", $acc_ver, "\n";
-	my $complement = @{ $gene{$key} }[1];
-	print "Complement		", $complement, "\n";
-	my  $gene = @{ $gene{$key} }[2];
-	print "Gene			", $gene, "\n";
-	my  $sequence = @{ $gene{$key} }[3];
-	print "Sequence		", $sequence, "\n";
-	my  $map = @{ $gene{$key} }[4];
-	print "Map			", $map, "\n";
-	my  $cod_start = @{ $gene{$key} }[5];
-	print "Cod Start		", $cod_start, "\n";	
-	my  $product = @{ $gene{$key} }[6];
-	print "Product			", $product, "\n";	
-	my  $protID = @{ $gene{$key} }[7];
-	print "Protein ID		", $protID, "\n";
-	my  $aa = @{ $gene{$key} }[8];
-	print "AA Sequence		", $aa, "\n";
+# foreach my $key (keys %gene)
+# {
+# 	#print "key $key value $genes{$key} \n";
+# 	#print "key $key value1 @{ $genes{$key} } \n"
+# 	print "Gene ID			", $key, "\n";
+# 	my $acc_ver = @{ $gene{$key} }[0];
+# 	print "AccesionVersion		", $acc_ver, "\n";
+# 	my $complement = @{ $gene{$key} }[1];
+# 	print "Complement		", $complement, "\n";
+# 	my  $gene = @{ $gene{$key} }[2];
+# 	print "Gene			", $gene, "\n";
+# 	my  $sequence = @{ $gene{$key} }[3];
+# 	print "Sequence		", $sequence, "\n";
+# 	my  $map = @{ $gene{$key} }[4];
+# 	print "Map			", $map, "\n";
+# 	my  $cod_start = @{ $gene{$key} }[5];
+# 	print "Cod Start		", $cod_start, "\n";	
+# 	my  $product = @{ $gene{$key} }[6];
+# 	print "Product			", $product, "\n";	
+# 	my  $protID = @{ $gene{$key} }[7];
+# 	print "Protein ID		", $protID, "\n";
+# 	my  $aa = @{ $gene{$key} }[8];
+# 	print "AA Sequence		", $aa, "\n";
+# 
+# 	print "@@@@@@@@@@@@@@@\n\n";  
+# 
+# }
 
-	print "@@@@@@@@@@@@@@@\n\n";  
-
-}
 
 
-
-foreach my $key (keys %cordinates) {
+foreach my $key (keys %cordinates_hash) {
 	print "###########    ", $key, "\n";
-	print $cordinates{$key};
+	my @aoa = @{$cordinates_hash{$key}};
+
+	print Dumper \@aoa;
+
+
+	for my $i ( 0 .. $#aoa ) {
+        my $row = $aoa[$i];
+        for my  $j ( 0 .. $#{$row} ) {
+            print "element $i $j is $row->[$j]\n";
+        }
+    }
+
 }
+
+
+
+
 
 1;
 exit;
