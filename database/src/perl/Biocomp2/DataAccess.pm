20b1edcb815086a56dea378e2f37ed83e0ca9c63
Igor Ruiz_De_Los_Mozos <ri001@hope.cryst.bbk.ac.uk>
2016-04-05 22:05:38 +0100

    ???? ERRROR

--- database/src/perl/Biocomp2/DataAccess.pm
+++ database/src/perl/Biocomp2/DataAccess.pm
@@ -9,7 +9,7 @@ sub bye {
   return "validating 44444";
 }
 
-sub get_gene_names {		my $dbname   = "ri001";	my $dbhost   = "hope.cryst.bbk.ac.uk";				#hope.cryst.bbk.ac.uk	my $dbsource = "dbi:mysql:database=$dbname;host=$dbhost";	my $username = "ri001";	my $password = "6xu1ornxo";	my $dbh = DBI->connect($dbsource, $username, $password) or die "Imposible conect to DataBase \n";	my $sql = 'SELECT gene_ID, acc_ver  FROM chromosome16_genes';	my $gene_ID = '';	my $acc_ver = '';	my %gene_names;    my $sth = $dbh->prepare($sql);    $sth->execute;        while(($gene_ID, $acc_ver) = $sth->fetchrow_array)    {    	#print "$gene_ID, $acc_ver \n";		$gene_names{$gene_ID}=$acc_ver;		}	return %gene_names;	}
+sub get_gene_names {	my $dbname   = "ri001";	my $dbhost   = "hope.cryst.bbk.ac.uk";					my $dbsource = "dbi:mysql:database=$dbname;host=$dbhost";	my $username = "ri001";	my $password = "6xu1ornxo";		my $dbh = DBI->connect($dbsource, $username, $password) or die "Imposible conect to DataBase \n";		my $sql = 'SELECT gene_ID, acc_ver  FROM chromosome16_genes';	my $gene_ID = '';	my $acc_ver = '';	my %gene_names;	    my $sth = $dbh->prepare($sql);    $sth->execute;        while(($gene_ID, $acc_ver) = $sth->fetchrow_array)    {    			$gene_names{$gene_ID}=$acc_ver;		}		return %gene_names;	}
 
 sub get_genes {
 
@@ -126,7 +126,7 @@ foreach my $key (keys %gene_hash)
 
 
 
-}
+
 }
 
 1; 
