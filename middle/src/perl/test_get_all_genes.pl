474a6ecc9078d40617310060474b1a6a23beeb59
Jurn Ho <jurn@magicmonster.com>
2016-04-05 21:37:06 +0100

    test

--- middle/src/perl/test_get_all_genes.pl
+++ middle/src/perl/test_get_all_genes.pl
@@ -5,5 +5,8 @@ use warnings;
 
 use Biocomp2::Middle;
 
-print Biocomp2::Middle::get_all_genes()
-
+my %genes = Biocomp2::Middle::get_all_genes();
+foreach my $key ( keys %genes )
+{
+  print "key: $key, value: $genes{$key}\n";
+}
