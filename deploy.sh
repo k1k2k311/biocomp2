c26b94422e4aacd302b08f5b083c15f19dd2ea55
ASIMINA EFSTRATIOU <ea001@hope.cryst.bbk.ac.uk>
2016-04-07 19:00:49 +0100

    changed the line order

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+mkdir -p ~/WWW/cgi-bin/Biocomp2/
 cp frontend/src/cgi/hello.pl ~/WWW/cgi-bin/
-mkdir ~/WWW/cgi-bin/Biocomp2/
 cp middle/src/perl/Biocomp2/Middle.pm ~/WWW/cgi-bin/Biocomp2/
 cp database/src/perl/Biocomp2/DataAccess.pm ~/WWW/cgi-bin/Biocomp2/
 chmod -R 755 ~/WWW/cgi-bin/
