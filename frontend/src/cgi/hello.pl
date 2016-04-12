4381f27bcf4c67febe21d8986166a77047a215a0
ASIMINA EFSTRATIOU <ea001@hope.cryst.bbk.ac.uk>
2016-04-12 19:11:04 +0100

    removed links2

--- frontend/src/cgi/hello.pl
+++ frontend/src/cgi/hello.pl
@@ -3,7 +3,6 @@ use CGI;
 use Biocomp2::Middle;
 $cgi = new CGI;
 print $cgi->header();
-my $param = $cgi->param('param');
 my $x = Biocomp2::Middle::hello();
 my %genes = Biocomp2::Middle::get_all_genes();
 my %details = Biocomp2::Middle::get_genes();
@@ -17,7 +16,7 @@ print <<__EOF;
 __EOF
 foreach my $key ( keys %genes )
 {
-	print "<li>key:<a href="./cgi-bin/Biocomp2/script.cgi?param=$key">$key</a> </li>,  value: $genes{$key} \n";
+	print "<li>key:$key </li>,  value: $genes{$key} \n";
 }
 print "</body> </html>";
 
