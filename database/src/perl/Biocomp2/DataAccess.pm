8d5e5a63aabf608586f0880fa2304d493ab1a33a
Igor Ruiz de los Mozos <i.mozos@ucl.ac.uk>
2016-04-14 19:35:09 +0100

    Add gene details sub

--- database/src/perl/Biocomp2/DataAccess.pm
+++ database/src/perl/Biocomp2/DataAccess.pm
@@ -4,12 +4,76 @@ use warnings;
 use DBI;
 
 
+sub gene_details {
 
-sub bye {
-  return "validating 44444";
+
+	my $dbname   = "ri001";
+	my $dbhost   = "hope.cryst.bbk.ac.uk";				#hope.cryst.bbk.ac.uk
+	my $dbsource = "dbi:mysql:database=$dbname;host=$dbhost";
+	my $username = "ri001";
+	my $password = "6xu1ornxo";
+
+	my $dbh = DBI->connect($dbsource, $username, $password) or die "Imposible conect to DataBase \n";
+
+
+    #my $gene_ID = @_;
+	my $sql = 'SELECT acc_ver, gene, map, product, protID  FROM chromosome16_genes WHERE gene_ID= 13937341';
+	my $acc_ver = '';
+	my $gene = '';
+	my $product = '';
+	my protID = '';
+
+	my @gene_details;
+
+
+	@gene_details= $dbh ->selectrow_array($sql);
+
+
+    foreach $element (@gene_details) {
+
+        print $element;
+    }
+	return @gene_names;
+
+
+
+
+
 }
 
-sub get_gene_names {	my $dbname   = "ri001";	my $dbhost   = "hope.cryst.bbk.ac.uk";					my $dbsource = "dbi:mysql:database=$dbname;host=$dbhost";	my $username = "ri001";	my $password = "6xu1ornxo";		my $dbh = DBI->connect($dbsource, $username, $password) or die "Imposible conect to DataBase \n";		my $sql = 'SELECT gene_ID, acc_ver  FROM chromosome16_genes';	my $gene_ID = '';	my $acc_ver = '';	my %gene_names;	    my $sth = $dbh->prepare($sql);    $sth->execute;        while(($gene_ID, $acc_ver) = $sth->fetchrow_array)    {    			$gene_names{$gene_ID}=$acc_ver;		}		return %gene_names;	}
+
+
+sub get_gene_names {
+	my $dbname   = "ri001";
+	my $dbhost   = "hope.cryst.bbk.ac.uk";				
+	my $dbsource = "dbi:mysql:database=$dbname;host=$dbhost";
+	my $username = "ri001";
+	my $password = "6xu1ornxo";
+	
+	my $dbh = DBI->connect($dbsource, $username, $password) or die "Imposible conect to DataBase \n";
+	
+	my $sql = 'SELECT gene_ID, acc_ver  FROM chromosome16_genes';
+	my $gene_ID = '';
+	my $acc_ver = '';
+	my %gene_names;
+	
+    my $sth = $dbh->prepare($sql);
+    $sth->execute;
+    
+    while(($gene_ID, $acc_ver) = $sth->fetchrow_array)
+    {
+    	
+
+		$gene_names{$gene_ID}=$acc_ver;
+
+	
+	}
+	
+	return %gene_names;
+	
+}
+
+
 
 sub get_genes {
 
