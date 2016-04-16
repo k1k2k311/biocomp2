9346249e861cc795a8976ecdc8b1c48fc3123be3
Igor Ruiz de los Mozos <i.mozos@ucl.ac.uk>
2016-04-16 14:25:18 +0100

    Add comments

--- database/src/perl/split.pl
+++ database/src/perl/split.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use DBI;
 use Data::Dumper qw(Dumper);
-use Biocomp2::DataAccess;
+#use Biocomp2::DataAccess;
 
 # Open data file 
 my $data_file = "../chrom_CDS_16";
@@ -51,13 +51,13 @@ while ($entry = get_entry($file) ) {
 	my $acc_ver = '';
 	my $locus = '';
 	my @annotation; 
-	@annotation = split /^/m, $annotation;			# split to array by start of the line
+	@annotation = split /^/m, $annotation;			    # split to array by start of the line
 	#print "Size: ",scalar @annotation,"\n";
 	#print @annotation;
 	
 	for my $annotation_line (@annotation) {
 	#for my $annotation_line ($annotation) {
-		if($annotation_line =~ /^LOCUS/) {
+		if($annotation_line =~ /^LOCUS/) {              # Get whole line begining with LOCUS
 			$annotation_line =~ s/^LOCUS\s*//;
 			chomp($locus = $annotation_line);
 			#print $locus;
@@ -65,17 +65,17 @@ while ($entry = get_entry($file) ) {
 
 	
 		elsif($annotation_line =~ /^ACCESSION/) {
-			$annotation_line =~ s/^ACCESSION\s*//;
+			$annotation_line =~ s/^ACCESSION\s*//;      # Get whole line begining with ACCESION
 			chomp(my $accession = $annotation_line);
 			#print "Accesion	", $accession, "\n";
 		
 		
 		}	
-		elsif($annotation_line =~ /^VERSION/) {
+		elsif($annotation_line =~ /^VERSION/) {         # Get whole line begining with VERSION
 			$annotation_line =~ s/^VERSION\s*//;
 			my $version = $annotation_line;
 			#print $version;
-			if ( $version =~ m/^([A-Za-z0-9].*)\s*GI:(.*\n)/ ) {
+			if ( $version =~ m/^([A-Za-z0-9].*)\s*GI:(.*\n)/ ) {        # Get numbers and letters upstream 'GI:' and everithing downstream = gene_ID
 			
 				#print "VERSION  ", $1, "\n\n";
 	
@@ -90,16 +90,16 @@ while ($entry = get_entry($file) ) {
 
 	} # For annotation
 	
-	my ($cds, @cordinates, $complement, $gene, $map, $st_name, $cod_start, $product, $protID, $aa);
+	my ($cds, @cordinates, $complement, $gene, $map, $st_name, $cod_start, $product, $protID, $aa);         #Iniciate variables
 
 	foreach (my $features_line = $features){
 		
 		
 		
 		
-		if ($features_line =~ /\/map="(.*)"/){
+		if ($features_line =~ /\/map="(.*)"/){                  # match everything quoted after /map=
 			$map = $1;
-			#print "Map		", $map, "\n";
+			print "Map		", $map, "\n";
 		}
 		if($features_line =~ /CDS\s*(.[^\/]*?)\//m){			# Match everything (including \n) after CDS but / until /
 
@@ -115,7 +115,7 @@ while ($entry = get_entry($file) ) {
 				
 				#print "1   ", $1, "\n";
 				#print "2   ", $2, "\n";
-				$complement = 1;											# Assign TRUE to complement
+				$complement = 1;											# Assign TRUE to complement strand
 				print "COMPLEMENT ", $compl_count++, "\n";
 
 				my @cord = split /,/, $1;									# Split by ,
@@ -150,7 +150,7 @@ while ($entry = get_entry($file) ) {
 						#print "START		", $1, "\n";
 						#print "END    		", $2, "\n";					
 						my @add_cord = ($1, $2);
-						push @cordinates, [@add_cord];						
+						push @cordinates, [@add_cord];						# Add Start Stop cordinates
 						#print Dumper \@cordinates, "\n";
 					
 					}
@@ -163,32 +163,32 @@ while ($entry = get_entry($file) ) {
 			#print Dumper \@cordinates, "\n";								
 		
 		}
-		if($features_line =~ /\/gene="(.*)"/){
+		if($features_line =~ /\/gene="(.*)"/){                              # Match everything quoted after /gene=
 			$gene = $1;
 			#print "GENE		", $gene, "\n"; 
 				
 		}
-		if($features_line =~ /\/standard_name="(.*)"/){
+		if($features_line =~ /\/standard_name="(.*)"/){                     # Match everything quoted after /standard_name=
 			$st_name = $1;
 			#print "Std Name		", $st_name, "\n"; 
 		
 		}
-		if($features_line =~ /\/codon_start=(\d*)/){
+		if($features_line =~ /\/codon_start=(\d*)/){                        # Match everything quoted after /codon_start=
 			$cod_start = $1;
 			#print "Cod Start	", $cod_start, "\n";
 		}
-		if($features_line =~ /\/product="(.*)"/){
+		if($features_line =~ /\/product="(.*)"/){                           # Match everything quoted after /product=
 			$product = $1;
 			#print "Product  	", $product, "\n";
 		
 		}
-		if($features_line =~ /\/protein_id="(.*)"/){
+		if($features_line =~ /\/protein_id="(.*)"/){                        # Match everything quoted after /protein_id=
 			$protID = $1;
 			#print "ProteinID	", $protID, "\n";
 		
 		
 		}
-		if($features_line =~ /\/translation=(.[^"]*?)"/s){			# Match /translation= and everything but " until "
+		if($features_line =~ /\/translation=(.[^"]*?)"/s){			        # Match /translation= and everything but " until "
 			$aa = $1;
 			$aa =~ s/"//g;
 			#print "ProteinAA	", $aa, "\n";
@@ -275,7 +275,7 @@ sub open_file {
 	my $fin;
 	
 	unless(open ($fin, $fin_name)) {
-		print "\nCan't open fin_name, closing!!! \n";
+		print "\nCan't open $fin_name, closing!!! \n";
 		exit;
 	}
 	return $fin;
@@ -287,12 +287,12 @@ sub get_entry {
 
 	my ($file) = @_;
 	my ($entry) = '';
-	my ($reset_separator) = $/;
-	$/ = "//\n";
+	my ($reset_separator) = $/;                                     # Reset separator \n to special variable $/
+	$/ = "//\n";                                                    # Assign separator to //n "end of genebank entry"
 	
-	$entry = <$file>;
+	$entry = <$file>;                                               # Assign all the entry to scalar
 	
-	$/ = $reset_separator;
+	$/ = $reset_separator;                                          # Reset separator
 	
 	return $entry;
 	
@@ -301,19 +301,17 @@ sub get_entry {
 
 sub split_entry {
 
-	my $entry = $_[0];
-	#my $n = scalar(@_);
-	#print "SCALAR     ", $n;
+	my ($entry) = @_;
 	my $annot = '';
 	my $feat = '';
 	my $seq = '';
 
-	$entry =~  /^(LOCUS.*)(FEATURES.*)ORIGIN(.*)\/\/\n/s;
+	$entry =~  /^(LOCUS.*)(FEATURES.*)ORIGIN(.*)\/\/\n/s;           # Match everithing after LOCUS FEATURES and ORIGING until //n "end of genebank entry"
 	
 	$annot = $1;
 	$feat = $2;
 	$seq = $3;
-	$seq =~ s/\s*\d*//g;
+	$seq =~ s/\s*\d*//g;                                            # Remove all the spaces and digits from sequence
 	
 	return($annot, $feat, $seq);
 
