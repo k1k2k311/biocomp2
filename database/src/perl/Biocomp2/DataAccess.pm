c613ac97f6c3c618bf16c2a038f14ad9130c1959
Igor Ruiz_De_Los_Mozos <ri001@hope.cryst.bbk.ac.uk>
2016-04-18 00:30:52 +0100

    Query get coordinates

--- database/src/perl/Biocomp2/DataAccess.pm
+++ database/src/perl/Biocomp2/DataAccess.pm
@@ -7,8 +7,72 @@ use DBI;
 #SELECT gene_ID, product FROM chromosome16_genes WHERE product like '%acetyl%';  SELECT gene_ID, product FROM chromosome16_genes WHERE product like '%acl%' or gene_ID like '%5917707%';
 #mysql> SELECT gene_ID, acc_ver, gene, map, product, protID  FROM chromosome16_genes WHERE product ='F-box and leucine-rich repeat protein 16';
 
+sub get_coordinates_AOA {
 
 
+	my $dbname   = "ri001";
+	my $dbhost   = "hope.cryst.bbk.ac.uk";				#hope.cryst.bbk.ac.uk
+	my $dbsource = "dbi:mysql:database=$dbname;host=$dbhost";
+	my $username = "ri001";
+	my $password = "6xu1ornxo";
+
+	my $dbh = DBI->connect($dbsource, $username, $password) or die "Imposible conect to DataBase \n";
+
+
+
+    my ($gene_ID) = @_;
+	my $sql = "SELECT gene_ID, exon_count, COOR_start, COOR_end FROM coordinates WHERE gene_ID= $gene_ID";
+	#my $gene_ID = '';	
+	my $exon_count = '';	
+	my $COOR_start = '';
+	my $COOR_end = '';
+	my @cordinates;
+	my %cordinates_hash;
+
+    my $sth = $dbh->prepare($sql);
+    $sth->execute;
+
+    while(($gene_ID, $exon_count, $COOR_start, $COOR_end) = $sth->fetchrow_array)
+    {
+
+		my @add_cord = ($exon_count, $COOR_start, $COOR_end);
+		push @cordinates, [@add_cord];	
+		
+		
+	
+	}
+
+	return @cordinates;
+}
+
+
+
+sub get_coordinat {
+
+
+
+	my $dbname   = "ri001";
+	my $dbhost   = "hope.cryst.bbk.ac.uk";				#hope.cryst.bbk.ac.uk
+	my $dbsource = "dbi:mysql:database=$dbname;host=$dbhost";
+	my $username = "ri001";
+	my $password = "6xu1ornxo";
+
+	my $dbh = DBI->connect($dbsource, $username, $password) or die "Imposible conect to DataBase \n";
+
+
+
+    my ($gene_ID) = @_;
+	my $sql = "SELECT gene_ID, exon_count, COOR_start, COOR_end FROM coordinates WHERE gene_ID= $gene_ID";
+	my $exon_count = '';	
+	my $COOR_start = '';
+	my $COOR_end = '';
+	my @cordinates;
+	my %cordinates_hash;
+
+	my $gene_coordinates_ref =  $dbh ->selectrow_hashref($sql);
+
+	return $gene_coordinates_ref;
+}
 
 
 sub get_gene_details {
