8b37e42213207a94b9ea5c3cec6018e488e7a71f
Igor Ruiz de los Mozos <i.mozos@ucl.ac.uk>
2016-04-17 18:39:09 +0100

    Add subroutine to search

--- database/src/perl/Biocomp2/DataAccess.pm
+++ database/src/perl/Biocomp2/DataAccess.pm
@@ -5,6 +5,36 @@ use DBI;
 
 
 #SELECT gene_ID, product FROM chromosome16_genes WHERE product like '%acetyl%';  SELECT gene_ID, product FROM chromosome16_genes WHERE product like '%acl%' or gene_ID like '%5917707%';
+#mysql> SELECT gene_ID, acc_ver, gene, map, product, protID  FROM chromosome16_genes WHERE product ='F-box and leucine-rich repeat protein 16';
+
+
+sub get_search {
+
+
+	my $dbname   = "ri001";
+	my $dbhost   = "hope.cryst.bbk.ac.uk";				#hope.cryst.bbk.ac.uk
+	my $dbsource = "dbi:mysql:database=$dbname;host=$dbhost";
+	my $username = "ri001";
+	my $password = "6xu1ornxo";
+
+	my $dbh = DBI->connect($dbsource, $username, $password) or die "Imposible conect to DataBase \n";
+
+
+    my ($search) = @_;
+	my $sql = "SELECT gene_ID, acc_ver, gene, map, product, protID  FROM chromosome16_genes WHERE product LIKE '%$search%' or gene_ID LIKE '%$search%' or map LIKE '%$search%' or acc_ver LIKE '%$search%';;
+	my $acc_ver = '';
+	my $gene = '';
+	my $map = '';
+	my $product = '';
+	my $protID = '';
+
+	my $gene_details_ref =  $dbh ->selectrow_hashref($sql);
+
+	return $gene_details_ref;
+
+
+}
+
 
 sub get_gene_details {
 
@@ -19,7 +49,7 @@ sub get_gene_details {
 
 
     my ($gene_ID) = @_;
-	my $sql = "SELECT acc_ver, gene, map, product, protID  FROM chromosome16_genes WHERE gene_ID= $gene_ID";
+	my $sql = "SELECT gene_ID, acc_ver, gene, map, product, protID  FROM chromosome16_genes WHERE gene_ID= $gene_ID";
 	my $acc_ver = '';
 	my $gene = '';
 	my $map = '';
@@ -52,7 +82,7 @@ sub get_gene_names {
     my $sth = $dbh->prepare($sql);
     $sth->execute;
     
-    while(($gene_ID, $acc_ver) = $sth->fetchrow_array)
+    while(($gene_ID, $acc_ver) = $sth->fetchrow_array($sql));
     {
     	
 
