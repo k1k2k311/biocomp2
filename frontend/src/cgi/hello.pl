1a4101cfb51cd10fa0a7ab23759d6d4a98f56cd9
ASIMINA EFSTRATIOU <ea001@hope.cryst.bbk.ac.uk>
2016-04-18 13:20:22 +0100

    changed the page layout and the labels

--- frontend/src/cgi/hello.pl
+++ frontend/src/cgi/hello.pl
@@ -4,7 +4,6 @@ use URI::Escape qw( uri_escape );
 use Biocomp2::Middle;
 $cgi = new CGI;
 print $cgi->header();
-my $x = Biocomp2::Middle::hello();
 my %genes = Biocomp2::Middle::get_all_genes();
 my %details = Biocomp2::Middle::get_genes();
 print <<__EOF;
@@ -12,20 +11,20 @@ print <<__EOF;
 <head>
 <style>
 body {background-color:white;}
-h1   {color:blue;}
-tab1 {padding: 30px; color:black;}
+h1   {color:black; text-align: center;}
+tab1 {padding: 40px; color:black;}
 tab2 {color:grey;}
 </style>
-   <title>Hello World!</title>
+   <title>Chromosome 16</title>
 </head>
 <body>
-<h1>Hello World! $x</h1>
+<h1>Chromosome 16</h1>
 __EOF
 foreach my $key ( keys %genes )
 {
 	my $uri = 'prog1.cgi?data='.uri_escape($key);
    	my $html = escapeHTML($key);
-	print qq{<li>key:<tab2><a href="$uri">$html</a></tab2>,  <tab1>value:</tab1><tab2> $genes{$key} </tab2></li> \n};
+	print qq{<li>Gene identifier:<tab2><a href="$uri">$html</a></tab2>, Protein product name:<tab2>$genes{$key}</tab2></li> \n};
 
 }
 print "</body> </html>";
