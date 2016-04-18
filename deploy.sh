1fca567bfad619231c550018faed1a5ddf6e96dd
ASIMINA EFSTRATIOU <ea001@hope.cryst.bbk.ac.uk>
2016-04-18 16:31:19 +0100

    added new file and changed deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ~/WWW/cgi-bin/Biocomp2/
 cp frontend/src/cgi/hello.pl ~/WWW/cgi-bin/
+cp frontend/src/cgi/prog1.pl ~/WWW/cgi-bin/
 cp middle/src/perl/Biocomp2/Middle.pm ~/WWW/cgi-bin/Biocomp2/
 cp database/src/perl/Biocomp2/DataAccess.pm ~/WWW/cgi-bin/Biocomp2/
 chmod -R 755 ~/WWW/cgi-bin/
