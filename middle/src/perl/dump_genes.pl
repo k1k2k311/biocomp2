39d1e77d2fcce21aebbe9d223fc63b9332fc7097
Jurn Ho <jurn@magicmonster.com>
2016-04-21 20:48:07 +0100

    dump all genes

---
+++ middle/src/perl/dump_genes.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Biocomp2::Middle;
+
+my %genes = Biocomp2::Middle::get_genes();
+foreach my $gene_id ( keys %genes )
+{
+  print "gene_id: $gene_id\n";
+  dump_details($gene_id);
+}
+
+sub dump_details {
+  my ($gene_id) = @_;
+  my %details = Biocomp2::Middle::get_gene_details($gene_id);
+  my $accession_version = $details{'accession_version'};
+  print "  accession_version: $accession_version\n";
+  my $dna_sequence = $details{'dna_sequence'};
+  print "  dna sequence     : $dna_sequence\n";
+  print "  dna sequence len : ".length($dna_sequence)."\n";
+  my $exons = $details{'exons'};
+  my @exons = @{$exons};
+#  print "  exons: $exons\n";
+  for my $exon_hr (@exons) {
+    my %exon = %{$exon_hr};
+    my $exon_number = $exon{'number'};
+    my $start = $exon{'start'};
+    my $end = $exon{'end'};
+    print "    exon $exon_number from $start to $end\n";
+  }
+  my $aa_sequence = $details{'aa_sequence'};
+  print "  aa sequence      : $aa_sequence\n";
+}
