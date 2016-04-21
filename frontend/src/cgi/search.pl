025bbf41203706135bcf39e097e0dc7d57021a86
ASIMINA EFSTRATIOU <ea001@hope.cryst.bbk.ac.uk>
2016-04-21 20:51:05 +0100

    fixed details issues in the file

--- frontend/src/cgi/search.pl
+++ frontend/src/cgi/search.pl
@@ -4,8 +4,8 @@ use URI::Escape qw( uri_escape );
 use Biocomp2::Middle;
 $cgi = new CGI;
 print $cgi->header();
-my $gene_id = $query->param('search');
-my %result = Biocomp2::Middle::search($gene_id);
+my $random = $cgi->param('search');
+my %result = Biocomp2::Middle::search($random);
 print <<__EOF;
 <html>
 <head><link rel="stylesheet" type="text/css" href="http://student.cryst.bbk.ac.uk/~ea001/style.css">
@@ -25,11 +25,11 @@ foreach my $gene_id ( keys %result )
 {
 	my $uri = 'prog1.pl?data='.uri_escape($gene_id);
    	my $html = escapeHTML($gene_id);
-	my %details = %{$genes{$gene_id}};
-    	my $gene_name = $result{"name"};
-	my $gene_accession = $result{"accession_version"};
-	my $gene_locus= $result{"locus"};
- 	my $gene_product = $result{"product"};
+	my %gene_details = %{$result{$gene_id}};
+    	my $gene_name = $gene_details{"name"};
+	my $gene_accession = $gene_details{"accession_version"};
+	my $gene_locus= $gene_details{"locus"};
+ 	my $gene_product = $gene_details{"product"};
     	my $gene_label = "$gene_name $gene_product";
 	print "  <tr>\n";
 	print qq{<td><tab2><a href="$uri">$html</a></tab2></td> <td><tab2>$gene_name</tab2></td> <td><tab2>$gene_accession</tab2></td><td><tab2>$gene_locus</tab2></td> \n};
