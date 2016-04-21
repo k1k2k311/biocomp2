13e2f7d4f73f992fbcebb2f194c63644a2d68fe1
Jurn Ho <jurn@magicmonster.com>
2016-04-21 19:48:16 +0100

    sequence newlines

--- middle/src/perl/tkapp.pl
+++ middle/src/perl/tkapp.pl
@@ -40,13 +40,19 @@ sub handle_show_details() {
   my $product_txt = $details_win -> Label( -text => $details{'product'});
   my $protein_id_lbl = $details_win -> Label( -text => 'Protein ID:');
   my $protein_id_txt = $details_win -> Label( -text => $details{'protein_id'});
-
+  my $sequence_lbl = $details_win -> Label( -text => 'Sequence:');
+  my $sequence = $details{'sequence'};
+  # add newlines
+  my $sequence_with_lines = add_newlines($sequence);
+  my $sequence_txt = $details_win -> Label( -text => "$sequence_with_lines");
+  
   $gene_id_lbl-> grid($gene_id_txt);
   $gene_name_lbl -> grid ($gene_name_txt);
   $accession_lbl -> grid($accession_txt);
   $locus_lbl -> grid($locus_txt);
   $product_lbl -> grid($product_txt);
   $protein_id_lbl -> grid($protein_id_txt);
+  $sequence_lbl -> grid($sequence_txt);
 
   my $details_close_btn = $details_win->Button( -text => 'Close', 
                                                 -command => sub { $details_win -> destroy() } );
@@ -68,4 +74,19 @@ sub get_genes() {
     push @gene_labels, $gene_label;
   }
   return (\@gene_ids, \@gene_labels);
+}
+
+sub add_newlines() {
+  my ($sequence) = @_;
+  my $line_length = 60;
+  my @lines;
+  while ($sequence) {
+    my $line = substr $sequence, 0, $line_length;
+    $sequence = substr $sequence, $line_length;
+    print "line: $line\n";
+    push @lines, $line;
+  }
+  my $joined = join("\n",@lines);
+  print "joined: $joined\n";
+  return $joined;
 }
