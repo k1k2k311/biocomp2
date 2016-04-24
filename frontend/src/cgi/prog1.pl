28d65895b25c86c28ee28fdd3771f4570940b422
ASIMINA EFSTRATIOU <ea001@hope.cryst.bbk.ac.uk>
2016-04-24 20:20:39 +0100

    create array for the sequence for highlighting

--- frontend/src/cgi/prog1.pl
+++ frontend/src/cgi/prog1.pl
@@ -23,8 +23,7 @@ print <<__EOF;
 <input type="submit" value="Search"></form></tab3>
 
 __EOF
-print "semina";
-exit;
+
 print "<table>\n";
 # print out the result we got from the subroutine
 print "<thead>    <tr><td>Gene identifier</td><td>Protein product name</td><td>Genbank accession</td><td>Location</td></tr>";
@@ -32,23 +31,37 @@ print "<thead>    <tr><td>Gene identifier</td><td>Protein product name</td><td>G
 	my $gene_accession = $details{"accession_version"};
 	my $gene_locus= $details{"locus"};
 	my $dna_seq= $details{"dna_sequence"};
-exit;
 	my $length=length($dna_seq);
 	my $i=0;
-	my @array_seq;
+	my $j=$length;
+	my $dna_seq2=$dna_seq;
+	my @array_subseq;
 
 	
 	print "  <tr>\n";
 	print qq{<td><tab2>$gene_id</tab2></td> <td><tab2>$gene_name</tab2></td> <td><tab2>$gene_accession</tab2></td><td><tab2>$gene_locus</tab2></td> \n\n\n};
-exit;
+	print " </tr>\n";
+	
+	my @array_seq;	  		while($dna_seq2) {
+			my $base=chop($dna_seq2);   			push (@{$array_seq[$j]}, $base);
+			my $array=$array_seq[$j][0];
+			print $array;	
+			$j=$j-1;  		}	
+	
 	while ($i<=$length){
 		my $chopped= substr($dna_seq,$i,100);
-		push @array_seq, $chopped;
+		push @array_subseq, $chopped;
 		$i=$i+100;
 	}
-	print @array_seq;
+	
+	foreach my $element( @array_subseq)
+	{	
+		print "<tr><td>";
+		print $element;
+		print "</td></tr>";
+	}
 
-	print " </tr>\n";
+	
 print "</thead>";
 print "</table>\n";
 print "</body> </html>";
